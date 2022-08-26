09f7d7a68e897c365f62562f6b9e845590509d9b
c0c0pang <coc1789@naver.com>
2022-08-27 02:31:32 +0900

    create 구현

--- project-koh/frontend/src/App.js
+++ project-koh/frontend/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Header from './Header'
 import Nav from './Nav'
 import Article from './Article'
+import Create from './Create';
 import { useState } from 'react';
+
 function App() {
   // const _mode = useState("WELCOME");
   // const mode = _mode[0];
@@ -13,11 +15,12 @@ function App() {
   //useState는 사용시 크기가 2인 배열로 나타나며 첫번째 인덱스엔 정보, 두번째 인덱스엔 함수를 수정할 수 있는 정보가 담겨져 있음.
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId,setNextId]=useState(4);
+  const [topics,setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'javascript', body: 'javascript is ...' }
-  ]
+  ])
   let content = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
@@ -29,8 +32,20 @@ function App() {
       }
     }
   }
+  else if(mode === "CREATE"){
+    content = <Create onCreate={
+      (_title,_body)=>{
+        const newTopic={id:nextId,title:_title,body:_body};
+        const newTpoics=[...topics]
+        newTpoics.push(newTopic);
+        setTopics(newTpoics);
+        setMode("READ");
+        setId(nextId);
+        setNextId(nextId+1);
+    }}></Create>
+  }
   return (
-    <>
+    <div>
       <Header title="WEB" onChangMode={
         () => {
           //mode의 값을 바꿀려면 setMode를 사용하면 됨
@@ -44,7 +59,14 @@ function App() {
         }
       } />
       {content} 
-    </>
+      <br/>
+      <a href="create" onClick={
+        event=>{
+          event.preventDefault();
+          setMode("CREATE");
+        }
+      }>create</a>
+    </div>
   );
 }
 
