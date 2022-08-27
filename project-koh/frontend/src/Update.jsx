b10153776bd5e58c8113b690c1ef866d202b0ba6
c0c0pang <coc1789@naver.com>
2022-08-28 01:32:21 +0900

    update 메모

--- project-koh/frontend/src/Update.jsx
+++ project-koh/frontend/src/Update.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-
+//update 는 create 와 read 의 기능이 모두 들어가 있으며 value의 값을 상시 바꿀려면 state 를 이용해야 한다.
 function Update(props) {
     const [title,setTitle]=useState(props.title);
     const [body,setBody]=useState(props.body);
