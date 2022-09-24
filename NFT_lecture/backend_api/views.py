40762780b89dda4416acb9a7af840f7297bad02e
wook03 <koou4135@gmail.com>
2022-09-24 16:43:41 +0900

    lecture_api 구현

---
+++ NFT_lecture/backend_api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Lecture
+from rest_framework.views import APIView
+from .serializers import LectureSerializer
+
+class LectureListAPI(APIView):
+    def get(self, request):
+        queryset = Lecture.objects.all()
+        print(queryset)
+        serializer = LectureSerializer(queryset, many=True)
+        return Response(serializer.data)
--- project-koh/backend/board/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Board
-
-# Create your views here.
-def test(request):
-    return render(request, 'index.html')
