c220666dfe70851a7069be51dfcb64baed01b072
wook03 <koou4135@gmail.com>
2022-09-26 20:05:02 +0900

    read readme.txt

--- NFT_lecture/NFT_lecture/settings.py
+++ NFT_lecture/NFT_lecture/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = "NFT_lecture.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'board',
-        'USER': 'root',
-        'PASSWORD': '4135',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'board',
+    #     'USER': 'root',
+    #     'PASSWORD': '4135',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306'
+    # }
 }
 
 
--- NFT_lecture/backend_api/views.py
+++ NFT_lecture/backend_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from .models import Lecture
 from rest_framework.views import APIView
 from .serializers import LectureSerializer
+from django.urls import reverse
+
 
 class LectureListAPI(APIView):
     def get(self, request):
