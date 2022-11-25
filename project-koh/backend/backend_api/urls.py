df3ecae3449948a7052b9f8e8327115e1ff52c33
c0c0pang <coc1789@naver.com>
2022-11-25 23:48:04 +0900

    병합 완료

--- project-koh/backend/backend_api/urls.py
+++ project-koh/backend/backend_api/urls.py
@@ -11,11 +11,10 @@ router.register(r'user',views.UserViewSet)
 app_name = 'backend_api'
 urlpatterns = [
     path('get/category/', views.titleShow.as_view()), # 카테고리 목록
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     path('',include(router.urls)),  
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이미지 접근 http://localhost:8000/image/leaf_detaction.jpeg
-=======
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), # 이미지 접근 http://localhost:8000/image/leaf_detaction.jpeg
+
     path('',include(router.urls)),
     path('test',views.test.as_view()),
     
@@ -23,8 +22,5 @@ urlpatterns = [
     
     # path('reply/', views.get_reply),
     # path('post/', views.Lecture_create.as_view()),
-=======
     path('',include(router.urls)),    
->>>>>>> 741655a7 (fix: 병합 테스트)
 ]
->>>>>>> f189ab41 (refactor: api 주소 변경)
