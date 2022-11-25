bbeb32a7071518e8cfb6ef3830788fea9ef5c480
c0c0pang <coc1789@naver.com>
2022-11-25 19:30:32 +0900

    refactor: 함수화

--- project-koh/blockchain/MyLecture/scripts/mint-nft.js
+++ project-koh/blockchain/MyLecture/scripts/mint-nft.js
@@ -7,33 +7,51 @@ const API_KEY = process.env.API_KEY;
 // Define an Alchemy Provider
 const provider = new ethers.providers.AlchemyProvider('goerli', API_KEY)
 
-const contract = require("../artifacts/contracts/LectureNFT.sol/LectureNFT.json"); //for making abi
-//console.log(JSON.stringify(contract.abi)); to print your abi -> node scripts/mint-nft.js
-
-// Create a signer
-const privateKey = process.env.PRIVATE_KEY
-const signer = new ethers.Wallet(privateKey, provider)
-
-// Get contract ABI and address
-const abi = contract.abi
-const contractAddress = '0xfa1B05966456fF8eDA0B136A6b2f731C6a09F35C'
-
-// Create a contract instance
-const myNftContract = new ethers.Contract(contractAddress, abi, signer)
-
-// Get the NFT Metadata IPFS URL
-const tokenUri = "https://gateway.pinata.cloud/ipfs/QmYueiuRNmL4MiA2GwtVMm6ZagknXnSpQnB3z2gWbz36hP"
-
-// Call mintNFT function
-const mintNFT = async () => {
+const contract = require("../artifacts/contracts/LectureNFT.sol/LectureNFT.json"); 
+// const privateKey = process.env.PRIVATE_KEY
+    
+export const mintNFT = async (contractAddress,tokenUri,privateKey) => {
+    const signer = new ethers.Wallet(privateKey, provider)
+    const abi = contract.abi
+    const myNftContract = new ethers.Contract(contractAddress, abi, signer)
     let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
     await nftTxn.wait()
     console.log(`NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`)
 }
 
-mintNFT()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });java
+//for making abi
+//console.log(JSON.stringify(contract.abi)); to print your abi -> node scripts/mint-nft.js
+
+// Create a signer
+// const privateKey = process.env.PRIVATE_KEY
+// const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+// const abi = contract.abi
+// const contractAddress = '0xfa1B05966456fF8eDA0B136A6b2f731C6a09F35C'
+
+// Create a contract instance
+// const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+// Get the NFT Metadata IPFS URL
+// const tokenUri = "https://gateway.pinata.cloud/ipfs/QmYueiuRNmL4MiA2GwtVMm6ZagknXnSpQnB3z2gWbz36hP"
+
+// Call mintNFT function
+// const mintNFT = async (contractAddress,tokenUri) => {
+//     const privateKey = process.env.PRIVATE_KEY
+//     const signer = new ethers.Wallet(privateKey, provider)
+
+//     const abi = contract.abi
+
+//     const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+//     let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+//     await nftTxn.wait()
+//     console.log(`NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`)
+// }
+
+// mintNFT()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.error(error);
+//         process.exit(1);
+//     });java
