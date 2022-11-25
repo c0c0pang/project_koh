f566b4f8b947acfd4226f91a94287f80dcf42d7b
c0c0pang <coc1789@naver.com>
2022-11-25 23:48:04 +0900

    병합 완료

--- project-koh/backend/backend_api/urls.py
+++ project-koh/backend/backend_api/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('',include(router.urls)),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이미지 접근 http://localhost:8000/image/leaf_detaction.jpeg
 =======
@@ -32,15 +33,25 @@ urlpatterns = [
 >>>>>>> 6dda080b (refactor: api 주소 변경)
 >>>>>>> 7df054e5 (refactor: api 주소 변경)
 =======
+=======
+>>>>>>> b0f15e98 (병합 완료)
 <<<<<<< HEAD
 =======
 >>>>>>> 6dda080b (refactor: api 주소 변경)
 =======
 >>>>>>> 159a0272 (fix: 병합 테스트)
+<<<<<<< HEAD
 >>>>>>> 24706d93 (fix: 병합 테스트)
     path('',include(router.urls)),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 이미지 접근 http://localhost:8000/image/leaf_detaction.jpeg
 =======
+>>>>>>> b0f15e98 (병합 완료)
+=======
+
+>>>>>>> df3ecae3 (병합 완료)
+    path('',include(router.urls)),  
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), # 이미지 접근 http://localhost:8000/image/leaf_detaction.jpeg
+
     path('',include(router.urls)),
     path('test',views.test.as_view()),
     
@@ -50,15 +61,23 @@ urlpatterns = [
     # path('post/', views.Lecture_create.as_view()),
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 159a0272 (fix: 병합 테스트)
 =======
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     path('',include(router.urls)),    
 >>>>>>> 741655a7 (fix: 병합 테스트)
 >>>>>>> 7df054e5 (refactor: api 주소 변경)
+=======
+=======
+>>>>>>> df3ecae3 (병합 완료)
+    path('',include(router.urls)),    
+>>>>>>> b0f15e98 (병합 완료)
 ]
+<<<<<<< HEAD
 >>>>>>> f189ab41 (refactor: api 주소 변경)
 <<<<<<< HEAD
 >>>>>>> a46d4209 (refactor: api 주소 변경)
@@ -74,4 +93,9 @@ urlpatterns = [
 ]
 >>>>>>> f189ab41 (refactor: api 주소 변경)
 >>>>>>> 6dda080b (refactor: api 주소 변경)
+<<<<<<< HEAD
 >>>>>>> 7df054e5 (refactor: api 주소 변경)
+=======
+=======
+>>>>>>> df3ecae3 (병합 완료)
+>>>>>>> b0f15e98 (병합 완료)
